db45ba66ad198a506aabe82e13e659d4b5a85c7d
Udbhav9628 <udbhav9628@gmail.com>
2023-03-05 19:39:01 +0530

    websockets

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const { WebSocketServer } = require("ws");
+const express = require("express");
+const bodyParser = require("body-parser");
+const server = require("ws").Server;
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new server({ port: 8080 });
+const app = express();
 
+app.use(bodyParser.json());
+
+//WebSocket Server
 wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
-
   ws.on("message", function message(data) {
     console.log("received: %s", data);
+    // wss.clients.forEach(function e(Clint) {
+    //   Clint.send(data);
+    // });
   });
 
-  ws.send("Hello From Server");
+  ws.on("close", () => {
+    console.log("One Clint Disconnected");
+  });
+
+  ws.on("error", console.error);
+
+  console.log("New Clint Connected");
+});
+
+// Http Server
+app.listen("8000", () => {
+  console.log("Server Is Running on PORT 8000");
 });
 
 console.log("WebSocket is Running on Port 8080");
