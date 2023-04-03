79567009d55f127322e8aa4edc65ab20af93ffa2
Udbhav9628 <udbhav9628@gmail.com>
2023-04-03 15:26:04 +0530

    making htttps on ec2

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const fs = require("fs");
 const Server = require("http").createServer(app);
+
+const file = fs.readFileSync("./B354820E767F0F0757EACB392ABAAF93.txt");
 
 const io = require("socket.io")(Server, {
   cors: {
@@ -13,6 +16,15 @@ const io = require("socket.io")(Server, {
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get(
+  "/.well-known/pki-validation/B354820E767F0F0757EACB392ABAAF93.txt",
+  (req, res) => {
+    return res.sendFile(
+      "/home/ec2-user/server/Chess-Video-Chat-and-Play-Chess-Server/B354820E767F0F0757EACB392ABAAF93.txt"
+    );
+  }
+);
 
 app.get("/", (req, res) => {
   return res.status(200).send("App Is Running");
